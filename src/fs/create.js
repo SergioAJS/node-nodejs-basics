13a1ac222fb297cf4d4aa7fe3712f5d0aa5815a6
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2024-10-06 21:23:14 +0500

    feat: implement create.js function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import { open, close, writeFile } from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
+  open('src/fs/files/fresh.txt', 'wx', (err, fd) => {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        console.error('FS operation failed');
+        return;
+      }
+  
+      throw err;
+    }
+  
+    try {
+      writeFile('src/fs/files/fresh.txt', 'I am fresh and young', err => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    } finally {
+      close(fd, (err) => {
+        if (err) throw err;
+      });
+    }
+  }); 
 };
 
 await create();
