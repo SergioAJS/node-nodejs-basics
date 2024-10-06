13178a949a80047547ac0c330d10361fa2bedd33
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2024-10-06 22:40:10 +0500

    feat: implement read.js function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import { access, constants, readFile } from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    readFile('src/fs/files/fileToRead.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error('FS operation failed');
+            return;
+        }
+        console.log(data);
+    })
 };
 
 await read();
