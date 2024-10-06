bddab0987c9bbb434c1176c648faa8a3bd36dce3
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2024-10-06 21:59:08 +0500

    feat: implement copy.js function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import { cp, access, constants } from 'node:fs';
+
+const file = 'src/fs/files_copy';
+
 const copy = async () => {
-    // Write your code here 
+    access(file, constants.F_OK, isNotExist => {
+        isNotExist ?
+        access('src/fs/files', constants.F_OK, isNotExist => {
+            if (isNotExist) {
+                console.error('FS operation failed');
+                return;
+            } 
+            cp('src/fs/files', file, {recursive: true}, err => {
+                if (err) {
+                    console.error(err);
+                }
+            })
+        }) :
+        console.error('FS operation failed');
+    })
 };
 
 await copy();
