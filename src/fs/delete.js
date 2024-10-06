976881222f192e0f586348a3d9e428d729e9bba7
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2024-10-06 22:24:28 +0500

    feat: implement delete.js function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import { access, constants, unlink } from 'node:fs';
+
 const remove = async () => {
-    // Write your code here 
+    access('src/fs/files/fileToRemove.txt', constants.F_OK, isNotExist => {
+        isNotExist ?
+        console.error('FS operation failed') :
+        unlink('src/fs/files/fileToRemove.txt', err => {
+            if (err) throw err;
+        })
+    })
 };
 
 await remove();
