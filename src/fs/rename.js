814ea634cfea75773682be9902ee644170aacfcb
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2024-10-06 22:16:43 +0500

    feat: implement rename.js function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import { access, constants, rename as fsRename } from 'node:fs';
+
+
 const rename = async () => {
-    // Write your code here 
+    access('src/fs/files/wrongFilename.txt', constants.F_OK, isNotExist => {
+        isNotExist ? 
+        console.error('FS operation failed') :
+        access('src/fs/files/properFilename.md', constants.F_OK, isNotExist => {
+            isNotExist ? fsRename('src/fs/files/wrongFilename.txt', 'src/fs/files/properFilename.md', err => {
+                if (err) throw err;
+            }) :
+            console.error('FS operation failed');
+        })
+    })
 };
 
 await rename();
