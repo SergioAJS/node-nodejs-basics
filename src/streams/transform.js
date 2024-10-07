15fadea67a0689dd8b934e91286a8939af1b46e9
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2024-10-07 23:31:36 +0500

    feat: implement stream transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,10 @@
+import process from 'node:process';
+
 const transform = async () => {
-    // Write your code here 
+    await process.stdin.on('data', async data => {
+        data = await data.toString().split('').reverse().join('').trimStart();
+        await process.stdout.write(data + '\n\n');
+    })
 };
 
 await transform();
