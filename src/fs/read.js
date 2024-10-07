4777f7d530e0a050d9d654a88ab2862eaf4d03dd
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2024-10-07 07:43:19 +0500

    refactor: use fsPromises, path and Error

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,13 +1,21 @@
-import { access, constants, readFile } from 'node:fs';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'node:fs/promises';
 
 const read = async () => {
-    readFile('src/fs/files/fileToRead.txt', 'utf8', (err, data) => {
-        if (err) {
-            console.error('FS operation failed');
-            return;
-        }
-        console.log(data);
-    })
+    try {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = dirname(__filename);
+        const filePath = path.join(__dirname, 'files', '/', 'fileToRead.txt');
+
+        const content = await readFile(filePath, { encoding: 'utf8' });
+        console.log(content);
+        return;
+    } catch (err) {
+        err = new Error('FS operation failed');
+        console.error(err.message);
+    }
 };
 
 await read();
