86e1da01b58965e96e52ff24c28a3dba6a18aaef
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2024-10-07 23:48:14 +0500

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto'
+import process from 'node:process';
+
 const calculateHash = async () => {
-    // Write your code here 
-};
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = path.join(__dirname, 'files', '/', 'fileToCalculateHashFor.txt');
+
+    const content = await readFile(filePath, { encoding: 'utf8' });
+
+    const hash = await createHash('sha256').update(content).digest('hex')
+
+    await process.stdout.write(hash + '\n');
+}
 
 await calculateHash();
