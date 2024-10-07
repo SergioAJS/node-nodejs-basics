b02a20288e2ab1cc2270fed1d678e6edf5d94e26
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2024-10-07 08:58:19 +0500

    feat: implement write stream function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createWriteStream } from 'node:fs';
+import { writeFile } from 'node:fs/promises';
+import process from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+    try {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = dirname(__filename);
+        const filePath = path.join(__dirname, 'files', '/', 'fileToWrite.txt');
+        const writeableStream = createWriteStream(filePath);
+
+        await writeFile(filePath, '');
+
+        process.stdin.on("data", data => {
+            writeableStream.write(`${data}`);
+        })
+
+    } catch (err) {
+        err = new Error('Couldn`t write to file');
+        console.error(err.message);
+    }
 };
 
 await write();
