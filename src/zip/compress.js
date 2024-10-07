678d8a05dc83d4a2c933172bab49c87c94d5193d
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2024-10-08 00:04:39 +0500

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = path.join(__dirname, 'files', '/', 'fileToCompress.txt');
+    const archivePath = path.join(__dirname, 'files', '/', 'archive.gz');
+    const gzip = createGzip();
+    const source = createReadStream(filePath);
+    const destination = createWriteStream(archivePath);
+    
+    await pipeline(source, gzip, destination, (err) => {
+      if (err) {
+        console.error('An error occurred:', err);
+        process.exitCode = 1;
+      }
+    });
 };
 
 await compress();
